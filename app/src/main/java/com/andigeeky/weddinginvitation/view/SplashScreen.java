2725cdffc5fb9e1514469e33acff27c9647d0ba0
mamatagelanee07 <gelaneem@gmail.com>
2017-12-30 00:17:45 +0530

    changed splash screen

--- app/src/main/java/com/andigeeky/weddinginvitation/view/SplashScreen.java
+++ app/src/main/java/com/andigeeky/weddinginvitation/view/SplashScreen.java
@@ -3,7 +3,8 @@ package com.andigeeky.weddinginvitation.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+
+import com.andigeeky.weddinginvitation.R;
 
 public class SplashScreen extends BaseActivity {
 
@@ -15,14 +16,11 @@ public class SplashScreen extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_splash);
         mHandler = new Handler();
-        mRunnable = new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashScreen.this, SignUpScreen.class));
-                finish();
-            }
+        mRunnable = () -> {
+            startActivity(new Intent(SplashScreen.this, SignUpScreen.class));
+            finish();
         };
     }
 
